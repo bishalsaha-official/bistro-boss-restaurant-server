a389230b86fb5bf9e001781032cdd0f6eb40656c
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-01 01:10:33 +0600

    Implement delete method for specific menu item

--- index.js
+++ index.js
@@ -137,6 +137,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/menu/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // get reviews data
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray()

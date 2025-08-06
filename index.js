66b6cdae01c088838e6ca66d9ff2e03f866b2688
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-06 22:07:43 +0600

    implement order stats

--- index.js
+++ index.js
@@ -265,6 +265,45 @@ async function run() {
             res.send({ users, menuItems, orders, revenue })
         })
 
+        // Using aggregate pipeline
+        app.get('/order-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await paymentCollection.aggregate([
+                {
+                    $unwind: '$menuIds'
+                },
+                {
+                    $lookup: {
+                        from: 'menu',
+                        localField: 'menuIds',
+                        foreignField: '_id',
+                        as: 'menuItems'
+                    }
+                },
+                {
+                    $unwind: '$menuItems'
+                },
+                {
+                    $group: {
+                        _id: '$menuItems.category',
+                        quantity: { $sum: 1 },
+                        revenue: { $sum: "$menuItems.price" }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        category: '$_id',
+                        quantity: '$quantity',
+                        revenue: '$revenue'
+                    }
+                }
+
+
+            ]).toArray()
+
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
